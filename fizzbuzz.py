f437af513dfcbfd389cb854223f55feeda684fab
안원모 <tommy45231@naver.com>
2024-06-04 20:32:00 +0900

    Feat: 3의배수 fizz처리

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,13 @@
 print('it works')
+
+def fizz_numbers(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0:
+            print("fizz")
+        else:
+            print(i)
+
+# 예시 사용
+number = int(input("숫자를 입력하세요: "))
+fizz_numbers(number)
+
